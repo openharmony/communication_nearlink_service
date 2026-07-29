553cd752fe0c08aeb7c893d10ad11b7ed46f7955
MTin <hitzrf2022@163.com>
2026-07-29 11:32:30 +0800

    修复测距功能中因mul-overflow导致的crash问题 Signed-off-by: MTin <hitzrf2022@163.com>

--- services/stack/src/cp/bsl/sle/hadm/src/hadm_sm.c
+++ services/stack/src/cp/bsl/sle/hadm/src/hadm_sm.c
@@ -637,8 +637,10 @@ static HadmSoundingIqData_S *HadmBuildIqDataToService(SLE_Addr_S *addr, HadmIqIn
     * 双端测距值 = (本端tof_result + 对端tof_result) * 0.03 / 2 - tofCalib * 2
     * 其中 tofCalib的含义：Calibration value(校准值) of ToF.
     */
-    iqData->dutTof = localIqInfo->tofResult * 3 / 100;  // 3/100是为了乘以光速，将时间转换成距离，详细说明见上面注释
-    iqData->rtdTof = remoteIqInfo->tofResult * 3 / 100;  // 3/100是为了乘以光速，将时间转换成距离，详细说明见上面注释
+    // 3/100是为了乘以光速，将时间转换成距离，详细说明见上面注释
+    iqData->dutTof = (unit16_t)((unit64_t)(localIqInfo->tofResult) * 3 / 100);  
+    // 3/100是为了乘以光速，将时间转换成距离，详细说明见上面注释
+    iqData->rtdTof = (unit16_t)((unit64_t)(remoteIqInfo->tofResult) * 3 / 100);  
     iqData->iqChnlNum = remoteIqInfo->iqChnlNum > localIqInfo->iqChnlNum ? localIqInfo->iqChnlNum :
                                                                           remoteIqInfo->iqChnlNum;
     iqData->iqData = (HadmReportIqData_S *)SDF_MemZalloc(sizeof(HadmReportIqData_S) * iqData->iqChnlNum);
