00965fe742d7facee388392044e159e5d568097f
tanhaoran2 <tanhaoran@huawei.com>
2026-08-06 16:15:35 +0800

    update: 更新文件 cm_api.c
    Signed-off-by: tanhaoran2 <tanhaoran@huawei.com>

--- services/stack/src/cp/bsl/sle/cm/link/src/cm_api.c
+++ services/stack/src/cp/bsl/sle/cm/link/src/cm_api.c
@@ -466,7 +466,7 @@ static void CM_ConnectUpdateParamReqInner(void *arg)
     }
     // 共存场景下，HID interval设置不能小于15ms
     uint16_t coexInterval = 0;
-    if (SleAccessHidCoexModeInterval(&coexInterval, &link->rmtAddr, updateParam.connIntervalMin, false)) {
+    if (SleAccessHidCoexModeInterval(&coexInterval, &link->rmtAddr, updateParam.connIntervalMin)) {
         updateParam.connIntervalMin = coexInterval;
         updateParam.connIntervalMax = coexInterval;
     }
