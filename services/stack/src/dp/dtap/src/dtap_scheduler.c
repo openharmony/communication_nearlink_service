7dce1bf04bcf1fd039cf93dc2999ec8e76fc0746
wuyike <1014898854@qq.com>
2026-08-14 18:09:08 +0800

    revert: 回退dtap_scheduler.c的VLA栈溢出修复
    将DTAP_SaveFragmentData和DTAP_ScheduleLcid中的两处SDF_MemZalloc堆分配回退为原始VLA变长数组写法。

--- services/stack/src/dp/dtap/src/dtap_scheduler.c
+++ services/stack/src/dp/dtap/src/dtap_scheduler.c
@@ -560,12 +560,7 @@ static bool DTAP_SendData(uint16_t lcid, SDF_Buff_S *buff)
 
 static bool DTAP_SaveFragmentData(uint16_t lcid, SDF_Buff_S *buff[], uint32_t remainBuffCnt)
 {
-    DTAP_PendingPacket **pendingPkt = (DTAP_PendingPacket **)SDF_MemZalloc(
-        remainBuffCnt * sizeof(DTAP_PendingPacket *));
-    if (pendingPkt == NULL) {
-        DTAP_LOGE("malloc pendingPkt array failed, remainBuffCnt %u", remainBuffCnt);
-        return false;
-    }
+    DTAP_PendingPacket *pendingPkt[remainBuffCnt];
     for (uint32_t i = 0; i < remainBuffCnt; i++) {
         DTAP_PendingPacket *pkt = DTAP_CreatePacket(buff[i]);
         if (pkt == NULL) {
@@ -573,7 +568,6 @@ static bool DTAP_SaveFragmentData(uint16_t lcid, SDF_Buff_S *buff[], uint32_t re
                 DTAP_DestroyPacket((SDF_DListEntry_S *)pendingPkt[j]);
             }
             DTAP_LOGE("malloc pending packet failed, i %d, remainBuffCnt %d", i, remainBuffCnt);
-            SDF_MemFree(pendingPkt);
             return false;
         }
         pkt->isSplited = true;
@@ -586,7 +580,6 @@ static bool DTAP_SaveFragmentData(uint16_t lcid, SDF_Buff_S *buff[], uint32_t re
             DTAP_DestroyPacket((SDF_DListEntry_S *)pendingPkt[i]);
         }
         DTAP_LOGE("get fragment channel failed, lcid %d", lcid);
-        SDF_MemFree(pendingPkt);
         return false;
     }
 
@@ -600,10 +593,8 @@ static bool DTAP_SaveFragmentData(uint16_t lcid, SDF_Buff_S *buff[], uint32_t re
             lcid, fragmentChannel->srcTcid);
         SDF_DListDestroy(&fragmentChannel->pktList, DTAP_DestroyPacket);
         SDF_MemFree(fragmentChannel);
-        SDF_MemFree(pendingPkt);
         return false;
     }
-    SDF_MemFree(pendingPkt);
     DTAP_LOGD("save fragment data success, lcid %d, remainBuffCnt %d", lcid, remainBuffCnt);
     return true;
 }
@@ -633,13 +624,8 @@ static bool DTAP_ScheduleLcid(DTAP_PriorityQueue *q, DTAP_LcidNode *lcidNode, DT
         }
 
         uint32_t fragmentCnt = DLI_GetDataFragmentNums(pkt->buff);
-        SDF_Buff_S **fragmentBuf = (SDF_Buff_S **)SDF_MemZalloc(fragmentCnt * sizeof(SDF_Buff_S *));
-        if (fragmentBuf == NULL) {
-            DTAP_LOGE("malloc fragmentBuf array failed, fragmentCnt %u", fragmentCnt);
-            return false;
-        }
+        SDF_Buff_S *fragmentBuf[fragmentCnt];
         if (!DTAP_SplitData(channelNode->lcid, pkt->buff, fragmentBuf, fragmentCnt)) {
-            SDF_MemFree(fragmentBuf);
             return false;
         }
         SDF_BuffFree(pkt->buff);
@@ -656,7 +642,6 @@ static bool DTAP_ScheduleLcid(DTAP_PriorityQueue *q, DTAP_LcidNode *lcidNode, DT
 
         DTAP_PriorityQueuePop(q, lcidNode, channelNode, pkt);
         if (sendCnt >= fragmentCnt) {
-            SDF_MemFree(fragmentBuf);
             continue;
         }
         if (!DTAP_SaveFragmentData(channelNode->lcid, &fragmentBuf[sendCnt], (fragmentCnt - sendCnt))) {
@@ -664,10 +649,8 @@ static bool DTAP_ScheduleLcid(DTAP_PriorityQueue *q, DTAP_LcidNode *lcidNode, DT
                 SDF_BuffFree(fragmentBuf[i]);
             }
             DTAP_LOGE("save fragment channel failed, lcid %d", channelNode->lcid);
-            SDF_MemFree(fragmentBuf);
             return false;
         }
-        SDF_MemFree(fragmentBuf);
         break;
     }
     return true;
