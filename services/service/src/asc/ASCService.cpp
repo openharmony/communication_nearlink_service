a9876b465bc010426cfc057c56f4d6ca0c257d08
q00849622 <qiuhuizhen@huawei.com>
2026-08-17 19:01:24 +0800

    单切双通话挂断后再次起播无声
    Signed-off-by: q00849622 <qiuhuizhen@huawei.com>

--- services/service/src/asc/ASCService.cpp
+++ services/service/src/asc/ASCService.cpp
@@ -4185,6 +4185,14 @@ void ASCService::ExcuteDelayStop(const RawAddress &device)
             if (streamType == AUDIO_STREAM_NONE) {
                 continue;
             }
+
+            ASCState state = GetASCStatus(info.addr_);
+            if (IsInStartProcess(State)) {
+                // 缓存本次配置，在进行中的音频流配置/流打开流程结束后，再触发停流流程
+                HILOGI("[ASCService] device %{public}s not started", GetEncryptAddr(device.GetAddress()).c_str());
+                GetStopBuff(info.addr_).push(streamType);
+                continue;
+            }
             StopPlayingExcute(info.addr_, streamType);
         }
     }
