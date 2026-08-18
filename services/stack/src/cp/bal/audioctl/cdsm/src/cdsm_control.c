c5a4ac4f0910b7eadcb0376b3181fb3838a55ce7
chenguofeng <chenchenlswl@gmail.com>
2026-08-18 20:52:40 +0800

    update: 更新文件 cdsm_control.c
    Signed-off-by: chenguofeng <chenchenlswl@gmail.com>

--- services/stack/src/cp/bal/audioctl/cdsm/src/cdsm_control.c
+++ services/stack/src/cp/bal/audioctl/cdsm/src/cdsm_control.c
@@ -359,7 +359,7 @@ static void CdsmReadMemberAddrHandle(CdsmCoopSet_S *coopSet, CdsmCoopSetMeb_S *s
         CdsmCoopSetMeb_S *meb = SDF_VectorElementAt(coopSet->mebs, index);
         CdsmRemoveCoopSetMember(coopSet->gid, &meb->addr);
     }
-    if (coopSet->mebs->size != coopSet->num) {
+    if ((coopSet->mebs->size != coopSet->num) && (coopSet->mebs->size <= UINT8_MAX)) {
         CP_LOG_ERROR("[CDSM] mebs size(%zu) mismatch num(%u) after addr read, correcting",
             coopSet->mebs->size, coopSet->num);
         coopSet->num = (uint8_t)coopSet->mebs->size;
