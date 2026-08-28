451bfa22ec709a22fc3771db68190a2df3b832cf
rosyyy778 <meilang2@huawei.com>
2026-08-28 20:22:50 +0800

    update: 更新文件 SleInterfaceProfileHidHost.h
    Signed-off-by: rosyyy778 <meilang2@huawei.com>

--- services/service/include/SleInterfaceProfileHidHost.h
+++ services/service/include/SleInterfaceProfileHidHost.h
@@ -93,10 +93,17 @@ public:
     virtual int HidHostGetReport(std::string device, uint8_t id, uint16_t size, const uint8_t type) = 0;
 
     /**
-     * @brief Hid Host Get Divece Info.
+     * @brief Hid Host Get Device Info.
      *
      */
     virtual int GetHidDeviceInfo(const RawAddress &device, int infoType) = 0;
+
+    /**
+     * @brief Hid Host Set Device Manufacturer Ability.
+     *
+     */
+    virtual int SetDeviceManufacturerAbility(
+        const RawAddress &device, const std::array<uint8_t, SLE_MANU_ABILITY_LEN> &manuAbility) = 0;
 };
 
 } // namespace Sle
