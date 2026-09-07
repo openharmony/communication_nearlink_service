b969457cb9ea39fc7e17110f5abec8bbdfd306a6
qiuhuizhen <qiuhuizhen@huawei.com>
2026-09-07 12:17:43 +0800

    微信来电通话不支持音量调节
    Signed-off-by: qiuhuizhen <qiuhuizhen@huawei.com>

--- services/service/src/ccp/CcpService.cpp
+++ services/service/src/ccp/CcpService.cpp
@@ -296,13 +296,6 @@ void CcpService::HandlePhoneStateChange(const NearlinkCallPhoneState &phoneState
 
 void CcpService::HandleVoipCallDetailChange(const Telephony::CallAttributeInfo &info)
 {
-    std::string bundleName =
-        ServiceManagerPluginInterface::GetInstance()->GetBundleName(BundleNameType::BUNDLE_NAME_WECHAT);
-    /* 拦截没有接入CallKit的voip */
-    if (!bundleName.empty() && bundleName == info.voipCallInfo.voipBundleName) {
-        HILOGD("[CcpService]wechat voip call is intercepted");
-        return;
-    }
     switch (info.callState) {
         case TelCallState::CALL_STATUS_DIALING:
         case TelCallState::CALL_STATUS_INCOMING:
