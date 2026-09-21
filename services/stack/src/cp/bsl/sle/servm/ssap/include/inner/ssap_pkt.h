5deba9922f330668ad815fe221e4a8dce42d9c82
fengpengyi <fengpengyi1@h-partners.com>
2026-09-21 21:38:47 +0800

    modify ssap mtu Signed-off-by: fengpengyi <fengpengyi1@h-partners.com>

--- services/stack/src/cp/bsl/sle/servm/ssap/include/inner/ssap_pkt.h
+++ services/stack/src/cp/bsl/sle/servm/ssap/include/inner/ssap_pkt.h
@@ -25,7 +25,11 @@ extern "C" {
 
 #define SSAP_EXCHANGE_INFO_PKT_LEN 6        // 信息交换报文长度
 #define SSAP_STACK_MTU_DEFAULT 251          // 星闪服务MTU缺省值
+#ifdef SSAP_STACK_MTU
+#define SSAP_STACK_MTU_MAX 512             // 星闪服务MTU最大值
+#else
 #define SSAP_STACK_MTU_MAX 1024             // 星闪服务MTU最大值
+#endif
 #define SSAP_REASSEM_MAX_SIZE (32 * 1024)   // 分包重组后报文最大长度
 #define SSAP_EXCHANGE_VERSION SSAP_VERSION_1_3 // 星闪服务Version号
 #define SSAP_PDU_BASE_LEN 2                 // msgCode+ctl长度
